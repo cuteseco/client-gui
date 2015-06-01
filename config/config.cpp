e667b2f0d5dfc17694bde2d88a71db23f1510f4c
Christian Höne <christianhoene@cuteseco.org>
2015-06-01 23:03:23 +0200

    added config (missing files)

---
+++ config/config.cpp
@@ -0,0 +1,452 @@
+/* Copyright (C) 2015  Christian Höne <christianhoene@cuteseco.org>
+ *
+ * This file is part of CuteSeCo.
+ *
+ * CuteSeCo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CuteSeCo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CuteSeCo.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von CuteSeCo.
+ *
+ * CuteSeCo ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU Affero General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * CuteSeCo wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU Affero General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU Affero General Public License zusammen mit
+ * diesem Programm erhalten haben.
+ * Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#undef _MYNAME
+#define _MYNAME QString("Config:: ")
+
+QNetworkProxy Config::my_proxy;
+bool Config::USE_TRAYICON=true;
+int Config::DEBUG_LEVEL=0;
+QTranslator* Config::TRANSLATOR_QT=NULL;
+QTranslator* Config::TRANSLATOR_APP=NULL;
+
+Config::Config(QObject *parent) :
+    QObject(parent)
+{
+}
+
+Config::~Config()
+{
+}
+
+/* :helper
+ * log proxy
+ */
+void Config::log(QString logtext, LOG_TYPE type)
+{
+    if (type > Config::DEBUG_LEVEL)
+        return;
+
+    QTextStream(stderr)
+            << QDateTime::currentDateTime().toString("dd.MM.yyyy HH:mm:ss")
+            << " "
+            << LoggerTypeDelegate::type2string(type)
+            << " "
+            << _MYNAME+logtext
+            << endl;
+}
+
+QString Config::getFullProgName()
+{
+    return QString("%1 %2Build:%3 %4 %5 (%6)")
+            .arg(PROJECT_PROGNAME)
+            .arg(PROJECT_VERSION)
+            .arg(PROJECT_BUILDNO)
+            .arg(_ARCHITECTURE)
+            .arg(_OS_TYPE)
+            .arg(PROJECT_STATE);
+}
+
+// proxy
+QNetworkProxy Config::loadProxy()
+{
+    log("loadProxy()", LOG_DEBUGDETAILINFO);
+
+    QNetworkProxy proxy;
+
+    switch(settings.value(SETTING_PROXYUSE).toInt())
+    {
+    case SETTINGSOPTION_NOPROXY:
+    {
+        log("no proxy", LOG_DEBUGINFO);
+
+        proxy.setType(QNetworkProxy::NoProxy);
+    }; break;
+    case SETTINGSOPTION_MANUALPROXY:
+    {
+        log("manual proxy", LOG_DEBUGINFO);
+
+        QUrl proxyurl(settings.value(SETTING_PROXYURL).toString());
+        if (proxyurl.isEmpty())
+        {
+            log("INVALID MANUAL PROXY. Fallback to default proxy.", LOG_ERROR);
+            proxy.setType(QNetworkProxy::DefaultProxy);
+        } else
+        {
+            proxy.setType(QNetworkProxy::HttpProxy);
+            proxy.setHostName(proxyurl.host());
+            proxy.setPort(proxyurl.port());
+            proxy.setUser(settings.value(SETTING_PROXYUSER, "").toString());
+            proxy.setPassword(settings.value(SETTING_PROXYPASSWORD,
+                                             ""
+                                             ).toString()
+                              );
+        }
+    }; break;
+    case SETTINGSOPTION_AUTOPROXY:
+    default:
+    {
+        log("default system proxy", LOG_DEBUGINFO);
+
+        QNetworkProxyFactory::setUseSystemConfiguration(true);
+
+        QList<QNetworkProxy> listOfProxies =
+                QNetworkProxyFactory::systemProxyForQuery(
+                    QNetworkProxyQuery(
+                        QUrl(QLatin1String("https://cuteseco.org"))
+                        )
+                    );
+
+        if (listOfProxies.isEmpty())
+            proxy.setType(QNetworkProxy::NoProxy);
+        else
+            proxy = listOfProxies.at(0);
+    }
+    }
+
+    return proxy;
+}
+
+void Config::initProxy()
+{
+    log("initProxy()", LOG_DEBUGDETAILINFO);
+
+    setProxy(loadProxy());
+}
+
+void Config::setProxy(QNetworkProxy proxy)
+{
+    log(QString("setProxy(QNetworkProxy proxy) %1")
+        .arg(proxy2String(proxy)),
+        LOG_DEBUGDETAILINFO
+        );
+    my_proxy = proxy;
+    QNetworkProxy::setApplicationProxy(my_proxy);
+}
+
+QNetworkProxy Config::getProxy()
+{
+    return my_proxy;
+}
+
+QString Config::proxy2String(QNetworkProxy proxy)
+{
+    QString proxytype("");
+
+    if (proxy.type() == QNetworkProxy::DefaultProxy)
+    {
+        proxytype = "(default) ";
+        proxy = my_proxy;
+    }
+
+    switch (proxy.type())
+    {
+    case QNetworkProxy::NoProxy:
+    {
+        return "no proxy";
+    }; break;
+    case QNetworkProxy::Socks5Proxy:
+    {
+        proxytype += "socks";
+    }; break;
+    case QNetworkProxy::HttpProxy:
+    {
+        proxytype += "http";
+    }; break;
+    default: proxytype += "unknown";
+    }
+
+    QString credentials;
+
+    if (!proxy.user().isEmpty() || !proxy.password().isEmpty())
+    {
+        if (!proxy.user().isEmpty())
+            credentials.append("<user>:");
+
+        if (!proxy.password().isEmpty())
+            credentials.append("<password>");
+
+        credentials.append("@");
+    }
+
+    return QString("%1://%2%3:%4")
+            .arg(proxytype)
+            .arg(credentials)
+            .arg(proxy.hostName())
+            .arg(proxy.port());
+}
+
+QString Config::getStoragePath()
+{
+    log("getStoragePath()", LOG_DEBUGDETAILINFO);
+
+    QString storagePath =
+            QStandardPaths::writableLocation(QStandardPaths::DataLocation);
+    QDir dir(storagePath);
+    if (!dir.exists())
+        dir.mkpath(storagePath);
+    return dir.path();
+}
+
+QString Config::getDownloadPath()
+{
+    log("getDownloadPath()", LOG_DEBUGDETAILINFO);
+
+    QString downloadPath =
+            QStandardPaths::writableLocation(QStandardPaths::DownloadLocation);
+    QDir dir(downloadPath);
+    if (!dir.exists())
+        dir.mkpath(downloadPath);
+
+    return dir.absolutePath()+"/";
+}
+
+QString Config::getDocumentPath()
+{
+    log("getDocumentPath()", LOG_DEBUGDETAILINFO);
+
+    QString documentPath =
+            QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+    QDir dir(documentPath);
+    if (!dir.exists())
+        dir.mkpath(documentPath);
+
+    return dir.absolutePath()+"/";
+}
+
+QString Config::getTempPath()
+{
+    log("getTempPath()", LOG_DEBUGDETAILINFO);
+
+    QString tempPath =
+            QStandardPaths::writableLocation(QStandardPaths::TempLocation);
+    QDir dir(tempPath);
+    if (!dir.exists())
+        dir.mkpath(tempPath);
+
+    return dir.absolutePath()+"/";
+}
+
+QString Config::getCachePath()
+{
+    log("getCachePath()", LOG_DEBUGDETAILINFO);
+
+    QString cachePath =
+            QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
+    QDir dir(cachePath);
+    if (!dir.exists())
+        dir.mkpath(cachePath);
+    return dir.path();
+}
+
+QString Config::getSettingsPath()
+{
+    log("getSettingsPath()", LOG_DEBUGDETAILINFO);
+
+    return settings.fileName();
+}
+
+QString Config::getLocale()
+{
+    log("getLocale()", LOG_DEBUGDETAILINFO);
+
+    QString defaultLocale = QLocale::system().name();
+    int pos = defaultLocale.lastIndexOf('_');
+    if (pos != -1)
+        defaultLocale.truncate(pos); // "de_DE" -> "de"
+    return defaultLocale;
+}
+
+QString Config::getOSType()
+{
+    return _OS_TYPE;
+}
+
+QString Config::getArchitecture()
+{
+    return _ARCHITECTURE;
+}
+
+QString Config::getOSName()
+{
+    QString OS = "not supported";
+
+#if defined(Q_OS_LINUX)
+    OS = "Linux";
+
+    QString programm = "/usr/bin/lsb_release";
+    if (QFileInfo(programm).exists())
+    {
+        QStringList programmArgs;
+        programmArgs << "-sd";
+        QProcess *linuxOS = new QProcess();
+        linuxOS->start(programm, programmArgs);
+        linuxOS->waitForFinished();
+        OS = linuxOS->readLine(1024);
+        OS.replace("\"", "");
+    } else
+    {
+        QString fileName = "/etc/SuSE-release";
+        if (QFileInfo(fileName).exists())
+        {
+            QFile file(fileName);
+            if (file.open(QFile::ReadOnly))
+                OS = file.readLine(1024);
+        }
+    }
+
+#elif defined(Q_OS_WIN32)
+    OS = "unsupported Windows";
+    // msdn: using system information
+    OSVERSIONINFOEX osvi;
+    ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+    if (GetVersionEx((OSVERSIONINFO *)&osvi))
+    {
+        bool workstation = osvi.wProductType == VER_NT_WORKSTATION;
+        SYSTEM_INFO si;
+        ZeroMemory(&si, sizeof(SYSTEM_INFO));
+        typedef void (WINAPI *PGNSI)(LPSYSTEM_INFO);
+        if (PGNSI pGNSI =
+                PGNSI(QLibrary("kernel32").resolve("GetNativeSystemInfo")))
+            pGNSI(&si);
+        else
+            GetSystemInfo(&si);
+        QString os;
+        switch ((osvi.dwMajorVersion << 8) + osvi.dwMinorVersion)
+        {
+        case 0x0500: os = workstation ? "2000 Professional" : "2000 Server";
+            break;
+        case 0x0501: os = osvi.wSuiteMask & VER_SUITE_PERSONAL ?
+                        "XP Home" : "XP Professional";
+            break;
+        case 0x0502:
+            if (GetSystemMetrics(SM_SERVERR2))
+                os = "Server 2003 R2";
+            else if (workstation &&
+                     si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64)
+                os = "XP Professional";
+            else
+                os = "Server 2003";
+            break;
+        case 0x0600: os = workstation ? "Vista" : "Server 2008";
+            break;
+        case 0x0601: os = workstation ? "7" : "Server 2008 R2";
+            break;
+        case 0x0602: os = workstation ? "8" : "Server 2012";
+            break;
+        case 0x0603: os = workstation ? "8.1" : "Server 2012 R2";
+            break;
+        default: os = "unknown";
+            break;
+        }
+        QString extversion((const QChar*)osvi.szCSDVersion);
+        return QString("Windows %1 %2(%3 bit)").arg(os)
+                .arg(extversion.isEmpty() ? "" : extversion + " ")
+                .arg(si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64 ?
+                         "64" : "32");
+    } else
+    {
+        switch( QSysInfo::WindowsVersion )
+        {
+        case QSysInfo::WV_2000:         OS = "Windows 2000 ";
+            break;
+        case QSysInfo::WV_XP:           OS = "Windows XP ";
+            break;
+        case QSysInfo::WV_2003:         OS = "Windows 2003 ";
+            break;
+        case QSysInfo::WV_VISTA:        OS = "Windows Vista ";
+            break;
+        case QSysInfo::WV_WINDOWS7:     OS = "Windows 7 ";
+            break;
+        case QSysInfo::WV_WINDOWS8:     OS = "Windows 8 ";
+            break;
+        case QSysInfo::WV_WINDOWS8_1:   OS = "Windows 8.1 ";
+            break;
+        default: OS = "Windows unknown ";
+            break;
+        }
+        OS += getArchitecture();
+    }
+#elif defined(Q_OS_OSX)
+    OS = "Mac OS";
+    QFile f("/System/Library/CoreServices/SystemVersion.plist");
+    if (f.open(QFile::ReadOnly))
+    {
+        QXmlStreamReader xml(&f);
+        while (xml.readNext() != QXmlStreamReader::Invalid)
+        {
+            if (!xml.isStartElement()
+                    || xml.name() != "key"
+                    || xml.readElementText() != "ProductVersion"
+                    )
+                continue;
+            xml.readNextStartElement();
+            OS = QString("Mac OS %1").arg(xml.readElementText());
+        }
+    }
+#endif
+
+    OS.replace("\n", "");
+    return OS;
+}
+
+bool Config::isAdministrator()
+{
+#if defined(Q_OS_LINUX)
+    return (getuid() ? false : true);
+#elif defined(Q_OS_WIN32)
+    BOOL b;
+    SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+    PSID AdministratorsGroup;
+    b = AllocateAndInitializeSid(
+                &NtAuthority,
+                2,
+                SECURITY_BUILTIN_DOMAIN_RID,
+                DOMAIN_ALIAS_RID_ADMINS,
+                0, 0, 0, 0, 0, 0,
+                &AdministratorsGroup);
+    if (b)
+    {
+        if (!CheckTokenMembership(NULL, AdministratorsGroup, &b))
+        {
+            b = FALSE;
+        }
+        FreeSid(AdministratorsGroup);
+    }
+
+    return(b);
+#endif
+}
---
+++ config/config.h
@@ -0,0 +1,153 @@
+/* Copyright (C) 2015  Christian Höne <christianhoene@cuteseco.org>
+ *
+ * This file is part of CuteSeCo.
+ *
+ * CuteSeCo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CuteSeCo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CuteSeCo.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von CuteSeCo.
+ *
+ * CuteSeCo ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU Affero General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * CuteSeCo wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU Affero General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU Affero General Public License zusammen mit
+ * diesem Programm erhalten haben.
+ * Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <QObject>
+#include <QSettings>
+#include <QStandardPaths>
+#include <QtNetwork>
+#include <QTranslator>
+
+#ifdef Q_OS_LINUX
+#include <unistd.h>
+#endif
+
+#include "buildno.h"
+#include "logger/logger.h"
+
+// proxy
+#define SETTING_PROXYUSE                "proxyuse"
+#define SETTINGSOPTION_NOPROXY          0
+#define SETTINGSOPTION_MANUALPROXY      1
+#define SETTINGSOPTION_AUTOPROXY        2
+#define SETTING_PROXYURL                "proxyurl"
+#define SETTING_PROXYUSER               "proxyuser"
+#define SETTING_PROXYPASSWORD           "proxypassword"
+
+// ui
+#define SETTING_SAVESTATE_VERSION       1
+#define SETTING_MAINSTATE               "mainstate"
+#define SETTING_MAINGEOMETRY            "mainwindowgeometry"
+#define SETTING_MAINPOSITION            "mainwindowsposition"
+
+// OS
+#if defined(Q_OS_LINUX)
+    #define _OS_TYPE            QString("GNU/Linux")
+    #define _USERDEVICE         QString("pc")
+#elif defined(Q_OS_ANDROID)
+    #define _OS_TYPE            QString("Android")
+    #define _USERDEVICE         QString("mobile")
+#elif defined(Q_OS_OSX)
+    #define _OS_TYPE            QString("OS_X")
+    #define _USERDEVICE         QString("mac")
+#elif defined(Q_OS_IOS)
+    #define _OS_TYPE            QString("iOS")
+    #define _USERDEVICE         QString("mobile")
+#elif defined(Q_OS_FREEBSD_KERNEL)
+    #define _OS_TYPE            QString("Free BSD")
+    #define _USERDEVICE         QString("pc")
+#elif defined(Q_OS_WIN32)
+    #define _OS_TYPE            QString("Windows")
+    #define _USERDEVICE         QString("pc")
+#elif defined(Q_OS_WINRT)
+    #define _OS_TYPE            QString("Windows RT")
+    #define _USERDEVICE         QString("mobile")
+#elif defined(Q_OS_WINPHONE)
+    #define _OS_TYPE            QString("Windows Phone")
+    #define _USERDEVICE         QString("mobile")
+#else
+    #define _OS_TYPE            QString("unknown")
+    #define _USERDEVICE         QString("unknown")
+#endif
+
+// ARCHITECTURE
+#if defined(Q_PROCESSOR_X86_64)
+    #define _ARCHITECTURE       QString("x86_64")
+#elif defined(Q_PROCESSOR_X86_32)
+    #define _ARCHITECTURE       QString("x86_32")
+#endif
+
+class Config : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Config(QObject *parent = 0);
+    ~Config();
+
+    static bool             USE_TRAYICON;
+    static int              DEBUG_LEVEL;
+    static QTranslator      *TRANSLATOR_QT;
+    static QTranslator      *TRANSLATOR_APP;
+
+    // log
+    static void log(QString logtext, LOG_TYPE type);
+
+    static QString getFullProgName();
+
+    // proxy
+    QNetworkProxy loadProxy();
+    void initProxy();
+    void setProxy(QNetworkProxy proxy);
+    static QNetworkProxy getProxy();
+    static QString proxy2String(QNetworkProxy proxy);
+
+    // path
+    static QString getStoragePath();
+    static QString getDownloadPath();
+    static QString getDocumentPath();
+    static QString getTempPath();
+    static QString getCachePath();
+    QString getSettingsPath();
+
+    // host specific
+    static QString getLocale();
+    static QString getOSType();
+    static QString getArchitecture();
+    static QString getOSName();
+    static bool isAdministrator();
+
+signals:
+
+public slots:
+
+private:
+    QSettings               settings;
+
+    static QNetworkProxy    my_proxy;
+};
+
+#endif // CONFIG_H
